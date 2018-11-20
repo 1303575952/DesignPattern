d1310327ed9d7f375fa6e523fb7f377126a642dd
1303575952 <1303575952@qq.com>
2018-11-20 17:53:36 +0800

    迪米特原则

--- src/main/java/com/felix/design/principle/demeter/Boss.java
+++ src/main/java/com/felix/design/principle/demeter/Boss.java
@@ -6,7 +6,7 @@ package com.felix.design.principle.demeter;
  */
 public class Boss {
 
-    public void commandCheckNumber(TeamLeader teamLeader){
+    public void commandCheckNumber(TeamLeader teamLeader) {
         teamLeader.checkNumberOfCourses();
     }
 
